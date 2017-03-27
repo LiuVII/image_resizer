5b8555c4d0db83aac9a87680061b3f0242b0867b
Mikhail Filipchuk <mfilipch@e1z3r2p19.42.us.org>
2017-03-27 00:07:24 -0700

    mainly completed

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# image_resizer
-Single endpoint web-sever that takes in an image and resizes it to the specified dimensions
+# Application 'image_resizer'
+Single endpoint web server takes in an image and resizes (scales) it to the specified dimensions
+
+## Setup
+To setup application simply git clone into any folder with reading and writing access rights.
+
+The application requires python 2 (version 2.7 or greater) with
+installed libraries specified in 'requirements.txt'.
+
+## Run
+To run the application cd to the application folder and simply execute:
+'''
+python app.py
+'''
+
+On a success the application will provide an URL path to the server:
+'''
+http://127.0.0.1:5000/
+'''
+
+## Inside the browser
+The application is tested with the Chrome browser
+(there could be performance issues with other browsers) 
+
+The starting page is 'index' page, so the whole address should be [url]/index:
+'''
+http://127.0.0.1:5000/index
+'''
+
+A simple form with 'choose file' button, 'height', 'width' fields and 'resize' button should appear
+
+## Resizing images
+Choose an image file to be uploaded
+(supported formats: 'bmp', 'png', 'jpg', 'jpeg', 'gif')
+
+Select the width and height you want to scale the image (accepted params from 0 to 999):
+- If both sizes specified image will scale to fit specified size (not preserving aspect ratio);
+- If only one size > 0, image will scale to the non-zero size, maintaining aspect ratio;
+- In case both sizes left blank or zero image size would be preserved;
+
+Uploaded images are collected inside 'uploads' folder, resized images can be found in 'resized' folder within the application folder
+
+## Additional info
+The application uses Pillow library and it's ANTIALIAS filter to perform overall good quality scaling.
+Still, performance can be increased depending on the specific input and the way of resizing.
+For example: upscaling of bit images could be done with an additional usage of SHARPEN and 
+EDGE_ENHANCE filters for better results.
+See more available filters:
+http://pillow.readthedocs.io/en/3.4.x/reference/ImageFilter.html
--- app.py
+++ app.py
@@ -1,35 +1,46 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for, send_from_directory, flash
+from flask import Flask, request, render_template, redirect, \
+	url_for, send_from_directory, flash
 from werkzeug.utils import secure_filename
 from PIL import Image
 
 # Initialize the Flask application
 app = Flask(__name__)
 
+# Setup session and secret key (error free execution of flash messages)
+# [In case we would want any for UI]
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 # The path to the upload directory
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# Create directory to stroe uploaded images
+
+# Create a directory to store uploaded images
 if not os.path.exists(UPLOAD_FOLDER):
 	try:
 		os.makedirs(UPLOAD_FOLDER)
 	except:
 		# error
+		print("Cannot create folder for uploaded images")
 		exit(1)
 
 # The path to the resized image directory
 RESIZED_FOLDER = "resized/"
 app.config['RESIZED_FOLDER'] = RESIZED_FOLDER
-# Create directory to store resized images
+
+# Create a directory to store resized images
 if not os.path.exists(RESIZED_FOLDER):
 	try:
 		os.makedirs(RESIZED_FOLDER)
 	except:
 		# error
+		print("Cannot create folder for resized images")
 		exit(1)
 
-# Extension that we are accepting to be uploaded
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+# Extensions we choose to accept (for example)
+# [PIL supports http://pillow.readthedocs.io/en/3.4.x/handbook/image-file-formats.html]
+ALLOWED_EXTENSIONS = set(['bmp', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
 
 # For a given file, return whether it's an allowed type or not
@@ -37,7 +48,7 @@ def allowed_file(filename):
 	return '.' in filename and \
 			filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-# For a given image, resize (scale) image and return it's filename:
+# For a given image, resize (scale) an image and return it's filename:
 def resize_image(filename, width, height):
 	
 	# Open image
@@ -45,67 +56,68 @@ def resize_image(filename, width, height):
 		img = Image.open(os.path.join(UPLOAD_FOLDER, filename))
 	except:
 		# error
+		print("Failed to load the image: %s" % (filename))
 		return 0
 	
-	# If both sizes are not set assign original size
+	# Assign original size if both sizes are not set,
+	# Scale to the set size maintaining aspect ratio if one of the sizes is not set
 	if width <= 0 and height <= 0:
 		width = img.size[0]
 		height = img.size[1]
-	# If one of the sizes not set scale to the set size maintaing aspect ratio 
-	elif not width:
+	elif img.size[1] and not width:
 		width = int(height * (float(img.size[0]) / (float(img.size[1]))))
-	elif not height:
+	elif img.size[0] and not height:
 		height = int(width * (float(img.size[1]) / (float(img.size[0]))))
 	
-	# Resize (scale) image (several filters can be used to provide the best quality)
+	# Resize (scale) image (antialias filter provides good quality)
 	img = img.resize((width, height), Image.ANTIALIAS)
 	
-	# Save image to the resized folder
+	# Save the image to the resized folder
 	resized_filename = "resized_" + filename
 	img.save(os.path.join(RESIZED_FOLDER, resized_filename))
 	return resized_filename
 
-# Initial route to our index page
+# The initial route to our index page
 @app.route('/index')
 def index():
 	return render_template('index.html')
 
-# Route to upload, save image locally and resize it
+# The route to upload, save image locally and resize it
 @app.route('/upload', methods=['POST'])
 def upload_file():
 	if request.method == 'POST':
 		
 		# Check if the post request has the file part
-		if 'file' not in request.files:
+		check = (('file' in request.files), ('height' in request.form), ('width' in request.form))
+		if not all(check):
 			# error
-			flash('No file part')
+			print('File(%d), Height(%d) and/or Width(%d) are missing' % (check))
 			return redirect(url_for('index'))
 		
 		# Get the values that were POSTed
 		file = request.files['file']
-		height = int(request.form['height'])
-		width = int(request.form['width'])
-		print('We are here')
+		height = int(request.form['height']) if request.form['height'] else 0
+		width = int(request.form['width']) if request.form['width'] else 0
 		
-		# Check if filename is empty
+		# Check if the filename is empty
 		if file.filename == '':
 			# error
-			flash('No selected file')
+			print('No file is selected')
 			return redirect(url_for('index'))
 		
-		# Check if file has an allowed extension
+		# Check if the file has an allowed extension
 		elif allowed_file(file.filename):
 			
 			# Make a secure filename (getting rid of directories in the filename)
 			filename = secure_filename(file.filename)
 			
-			# Save file locally
+			# Save the file locally
 			file.save(os.path.join(UPLOAD_FOLDER, filename))
 			
-			# Attempt to resize file
+			# Attempt to resize the file
 			resized_filename = resize_image(filename, width, height)
 			
-			# In case of success display resized file in the browser
+			# In the case of success display resized file in the browser
 			if resized_filename:
 				return redirect(url_for('resized_filename', filename=resized_filename))
 			#error
@@ -113,22 +125,24 @@ def upload_file():
 			return redirect(url_for('index'))
 		else:
 			# error
-			flash('File extension is not supported')
+			print('File extension is not supported')
 			return redirect(url_for('index'))
 	# error
-	flash('Not a POST method')
+	print('Not a POST method')
 	return redirect(url_for('index'))
 
-# Route to display resized file
+# The route to display thr resized file
 @app.route('/resized/<filename>')
 def resized_filename(filename):
-	print("DISPLAY ME")
+	# success
+	print("Displaying image")
 	return send_from_directory(RESIZED_FOLDER, filename)
 
-# Route to display uploaded file [NOT CURRENTLY USED]
+# THe route to display the uploaded file
+# [Currently not used]
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
 	return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0')
+	app.run(debug=True, host='127.0.0.1')
